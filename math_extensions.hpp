3ea12cb2773b5851d726be44529ae4761aa10603
PanieriLorenzo <32730730+PanieriLorenzo@users.noreply.github.com>
2022-07-25 15:34:54 +0200

    Update math_extensions.hpp

--- math_extensions.hpp
+++ math_extensions.hpp
@@ -1,1 +1,17 @@
+#include <cmath>
 
+namespace stoej {
+
+    float sign(float x)   { return std::copysign(1.0f, x); }
+    double sign(double x) { return std::copysign(1.0, x);  }
+
+    // ref: https://stackoverflow.com/a/16659263
+    float clamp(float x, float min, float max) {
+        const float t = x < min ? min : x;
+        return t > max ? max : t;
+    }
+    double clamp(double x, double min, double max) {
+        const double t = x < min ? min : x;
+        return t > max ? max : t;
+    }
+}
