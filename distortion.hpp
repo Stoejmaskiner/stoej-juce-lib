83089359a247fb59562cc53703bbe3e6504227f6
PanieriLorenzo <32730730+PanieriLorenzo@users.noreply.github.com>
2022-07-25 15:27:14 +0200

    Update distortion.hpp

--- distortion.hpp
+++ distortion.hpp
@@ -3,6 +3,9 @@
 float soft_sigmoid(float x)   { return std::tanhf(x); }
 double soft_sigmoid(double x) { return std::tanh(x);  }
 
-float soft_sigmoid(float x) {
-    std::sqrtf(std::tanhf(x*x)) * 
-}
+float medium_sigmoid(float x)   { return std::sqrtf(std::tanhf(x*x)) * std::copysign(1.0f, x); }
+double medium_sigmoid(double x) { return std::sqrt(std::tanh(x*x))   * std::copysign(1.0, x);  }
+
+float firm_sigmoid(float x)   { return std::sqrtf(std::sqrtf(std::tanhf(x*x*x*x))) * std::copysign(1.0f, x); }
+double firm_sigmoid(double x) { return std::sqrt(std::sqrt(std::tanh(x*x*x*x)))    * std::copysign(1.0, x);  }
+
