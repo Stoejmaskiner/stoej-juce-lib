6a27f94dbfde0019223fdcc5a5a9bcb92f8a2d51
PanieriLorenzo <32730730+PanieriLorenzo@users.noreply.github.com>
2022-07-25 15:13:31 +0200

    Update distortion.hpp

--- distortion.hpp
+++ distortion.hpp
@@ -1,3 +1,8 @@
-float soft_clip_32(float x, float thresh) {
-    
+#include <math.h>
+
+float soft_sigmoid(float x)   { return std::tanhf(x); }
+double soft_sigmoid(double x) { return std::tanh(x);  }
+
+float soft_sigmoid(float x) {
+    std::sqrtf(std::tanhf(x*x)) * 
 }
