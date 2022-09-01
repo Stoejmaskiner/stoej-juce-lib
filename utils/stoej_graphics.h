c20bc87bc06598683f10a4f21f1a379c3393b4ea
Lorenzo Panieri <panierilorenzo@gmail.com>
2022-09-01 02:34:38 +0200

    graphics

---
+++ utils/stoej_graphics.h
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    stoej_graphics.h
+    Created: 1 Sep 2022 1:50:31am
+    Author:  Lorenzo
+
+  ==============================================================================
+*/
+
+#pragma once
+
+namespace stoej {
+	constexpr double PT_2_PX = 1.333333;
+	constexpr double PX_2_PT = 0.75;
+}
