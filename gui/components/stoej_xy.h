eb9432228f4ca79acd1264ea548148f15bc44594
Lorenzo Panieri <gokk99@gmail.com>
2022-12-22 00:01:48 +0100

    started XY control

--- gui/components/stoej_xy.h
+++ gui/components/stoej_xy.h
@@ -9,3 +9,20 @@
 */
 
 #pragma once
+#include <JuceHeader.h>
+#include "gui/components/stoej_component.h"
+
+namespace stoej {
+    class XY : public stoej::FloatComponent<juce::Component> {
+    public:
+        void paint(juce::Graphics& g) override;
+        void mouseDown(const juce::MouseEvent& e) override;
+        void mouseDrag(const juce::MouseEvent& e) override;
+        void valueChanged(juce::Value& value) { this->repaint(); }
+
+        // TODO: interface with APVTS directly to avoid dependence on sliders
+
+        juce::Value getXValueObject();
+        juce::Value getYValueObject();
+    };
+}
