c40cc8e1cf6ff1e07639f57e255da072994faabe
Lorenzo Panieri <gokk99@gmail.com>
2023-01-07 12:54:32 +0100

    rewriting button drawing code WIP

--- gui/components/stoej_button.cpp
+++ gui/components/stoej_button.cpp
@@ -13,29 +13,13 @@
 #include "binary_data/stoej_Fonts.h"
 
 
-
-
-
-stoej::StoejButton::StoejButton(const juce::String& name, ButtonSize size, const juce::String& label, bool toggleable)
-	: StoejButton(name, size, label, "", nullptr, nullptr, toggleable, false, false) {}
-
-stoej::StoejButton::StoejButton(const juce::String& name, ButtonSize size, const juce::String& label_on, juce::String& label_off)
-	: StoejButton(name, size, label_on, label_off, nullptr, nullptr, true, true, false) {}
-
-stoej::StoejButton::StoejButton(const juce::String& name, ButtonSize size, std::unique_ptr<juce::Drawable> icon, bool toggleable)
-	: StoejButton(name, size, "", "", std::move(icon), nullptr, toggleable, false, true) {}
-
-stoej::StoejButton::StoejButton(const juce::String& name, ButtonSize size, std::unique_ptr<juce::Drawable> icon_on, std::unique_ptr<juce::Drawable> icon_off)
-	: StoejButton(name, size, "", "", std::move(icon_on), std::move(icon_off), true, true, true) {}
-
-
 stoej::StoejButton::StoejButton(
-	const juce::String& name,
+	const juce::String name,
 	ButtonSize size,
-	const juce::String& label_on,
-	const juce::String& label_off,
-	std::unique_ptr<juce::Drawable> icon_on,
-	std::unique_ptr<juce::Drawable> icon_off,
+	const juce::String label_on,
+	const juce::String label_off,
+	const std::unique_ptr<juce::Drawable>& icon_on,
+	const std::unique_ptr<juce::Drawable>& icon_off,
 	bool toggleable,
 	bool separate_on_off_looks,
 	bool use_icon)
@@ -44,13 +28,15 @@ stoej::StoejButton::StoejButton(
 	btn_size_(size),
 	label_on_(label_on),
 	label_off_(label_off),
-	icon_on_(std::move(icon_on)),
-	icon_off_(std::move(icon_off)),
+	icon_on_(icon_on),
+	icon_off_(icon_off),
 	separate_on_off_looks_(separate_on_off_looks),
 	use_icon_(use_icon)
 {
 	this->setClickingTogglesState(toggleable);
 	this->setToggleable(toggleable);
+	this->border_w_ = 1.0f;
+	this->border_c_ = juce::Colours::black;
 }
 
 
@@ -79,7 +65,16 @@ std::variant<float, stoej::DynamicSize2> stoej::StoejButton::getPreferredWidth()
 
 void stoej::StoejButton::paintButton(juce::Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
 {
-	auto r = getLocalFloatBounds();
+	bool pressed = this->isToggleable() && this->getToggleState() || !this->isToggleable() && shouldDrawButtonAsDown;
+
+	if (this->separate_on_off_looks_)
+		if (this->use_icon_) this->paintTwoIcons(g, pressed);
+		else this->paintTwoLabels(g, pressed);
+	else
+		if (this->use_icon_) this->paintOneIcon(g, pressed);
+		else this->paintOneLabel(g, pressed);
+	
+	/*
 	//this->setBorderWidth(1.0f);
 	//this->drawBorder(g);
 
@@ -91,17 +86,14 @@ void stoej::StoejButton::paintButton(juce::Graphics& g, bool shouldDrawButtonAsH
 		// TODO(Lorenzo): got here
 		if (this->getToggleState()) this->background_c_ = juce::Colours::black; //this->bounding_box_.fill_color = juce::Colours::black;
 		else this->background_c_ = juce::Colours::white;
-		
-
     // trigger button drawing logic 
 	} else {
 		//if (shouldDrawButtonAsDown) g.setColour(juce::Colours::red);
 		//if (!shouldDrawButtonAsDown) g.setColour(juce::Colours::black);
 		//g.drawRect(r);
 	}
-
-
 	this->drawBackground(g);
+
 
 	if (!this->use_icon_) {
 		if (this->getToggleState()) g.setColour(juce::Colours::white);
@@ -111,7 +103,7 @@ void stoej::StoejButton::paintButton(juce::Graphics& g, bool shouldDrawButtonAsH
 		g.drawText(this->label_on_, r, juce::Justification::centred);   // draw some placeholder text
 	}
 	else {
-		jassertfalse;	// TODO
+		this->icon_on_->drawWithin(g, this->icon_->getBounds().toFloat(), juce::RectanglePlacement(), 1.0);
 	}
 
 	this->setBorderWidth(1.0f);
@@ -120,16 +112,68 @@ void stoej::StoejButton::paintButton(juce::Graphics& g, bool shouldDrawButtonAsH
 
 	
 	
-	
+	*/
 }
 
-void stoej::StoejButton::paintOn()
+void stoej::StoejButton::paintOneIcon(juce::Graphics& g, bool pressed)
 {
+	auto r = this->getLocalFloatBounds();
+	if (pressed) {
+		this->background_c_ = juce::Colours::black;
+		this->drawBackground(g);
+		g.setColour(juce::Colours::white);
+		r.reduce(3 * dp_, 3 * dp_);
+		this->icon_on_->setBounds(r.toNearestInt());
+		// TODO: how to change the colour of this icon???
+		this->icon_on_->drawWithin(g, this->icon_on_->getBounds().toFloat(), juce::RectanglePlacement(), 1.0);
+	}
+	else {
+		this->background_c_ = juce::Colours::white;
+		this->drawBackground(g);
+		g.setColour(juce::Colours::black);
+		r.reduce(3 * dp_, 3 * dp_);
+		this->icon_on_->setBounds(r.toNearestInt());
+		this->icon_on_->drawWithin(g, this->icon_on_->getBounds().toFloat(), juce::RectanglePlacement(), 1.0);
+	}
+	this->drawBorder(g);
 }
 
-void stoej::StoejButton::paintOff()
+void stoej::StoejButton::paintTwoIcons(juce::Graphics& g, bool pressed)
 {
+	// TODO:
+	jassertfalse;
 }
+
+void stoej::StoejButton::paintOneLabel(juce::Graphics& g, bool pressed)
+{
+	if (pressed) {
+		this->background_c_ = juce::Colours::black;
+		this->drawBackground(g);
+		this->paintLabel(g, this->label_on_, juce::Colours::white);
+	}
+	else {
+		this->background_c_ = juce::Colours::white;
+		this->drawBackground(g);
+		this->paintLabel(g, this->label_on_, juce::Colours::black);
+	}
+	this->drawBorder(g);
+}
+
+void stoej::StoejButton::paintTwoLabels(juce::Graphics& g, bool pressed)
+{
+	// TODO:
+	jassertfalse;
+}
+
+void stoej::StoejButton::paintLabel(juce::Graphics& g, juce::String label, juce::Colour c) {
+	auto r = this->getLocalFloatBounds();
+	g.setColour(c);
+	g.setFont(stoej::get_font_archivo_narrow());
+	g.setFont(13.0f * dp_ * stoej::PT_2_PX);
+	g.drawText(label, r, juce::Justification::centred);
+}
+
+
 
 
 
--- gui/components/stoej_button.h
+++ gui/components/stoej_button.h
@@ -30,16 +30,20 @@ namespace stoej {
         enum ButtonSize {tiny, small, medium, large};
 
         // simple button with text label, optionally toggleable
-        StoejButton(const juce::String& name, ButtonSize, const juce::String& label, bool toggleable = false);
+        StoejButton(const juce::String name, ButtonSize size, const juce::String label, bool toggleable = false)
+            : StoejButton(name, size, label, "", nullptr, nullptr, toggleable, false, false) {}
 
         // toggleable button with separate on and off labels
-        StoejButton(const juce::String& name, ButtonSize, const juce::String& label_on, juce::String& label_off);
+        StoejButton(const juce::String name, ButtonSize size, const juce::String label_on, juce::String& label_off)
+            : StoejButton(name, size, label_on, label_off, nullptr, nullptr, true, true, false) {}
 
         // simple button with icon, optionally toggleable
-        StoejButton(const juce::String& name, ButtonSize, std::unique_ptr<juce::Drawable> icon, bool toggleable = false);
+        StoejButton(const juce::String name, ButtonSize size, const std::unique_ptr<juce::Drawable>& icon, bool toggleable = false)
+            : StoejButton(name, size, "", "", icon, nullptr, toggleable, false, true) {}
 
         // toggleable button with separate on and off icons
-        StoejButton(const juce::String& name, ButtonSize, std::unique_ptr<juce::Drawable> icon_on, std::unique_ptr<juce::Drawable> icon_off);
+        StoejButton(const juce::String name, ButtonSize size, const std::unique_ptr<juce::Drawable>& icon_on, std::unique_ptr<juce::Drawable> icon_off)
+            : StoejButton(name, size, "", "", icon_on, icon_off, true, true, true) {}
         
         std::variant<float, DynamicSize2> getPreferredHeight() override;
         std::variant<float, DynamicSize2> getPreferredWidth() override;
@@ -55,25 +59,31 @@ namespace stoej {
 
         // fat constructor
         StoejButton(
-            const juce::String& name,
+            const juce::String name,
             ButtonSize size,
-            const juce::String& label_on,
-            const juce::String& label_off,
-            const std::unique_ptr<juce::Drawable> icon_on_,
-            const std::unique_ptr<juce::Drawable> icon_off_,
+            const juce::String label_on,
+            const juce::String label_off,
+            const std::unique_ptr<juce::Drawable>& icon_on_,
+            const std::unique_ptr<juce::Drawable>& icon_off_,
             bool toggleable,
             bool separate_on_off_looks,
             bool use_icon);
 
         void paintButton(juce::Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override;
-        void paintOn();
-        void paintOff();
+
+        // TODO: consider inlining these?
+        void paintOneIcon(juce::Graphics& g, bool pressed);
+        void paintTwoIcons(juce::Graphics& g, bool pressed);
+        void paintOneLabel(juce::Graphics& g, bool pressed);
+        void paintTwoLabels(juce::Graphics& g, bool pressed);
+        void paintLabel(juce::Graphics& g, juce::String label, juce::Colour c);
+
 
         ButtonSize btn_size_;
-        const std::unique_ptr<juce::Drawable> icon_on_;
-        const std::unique_ptr<juce::Drawable> icon_off_;
-        const juce::String& label_on_;
-        const juce::String& label_off_;
+        const std::unique_ptr<juce::Drawable>& icon_on_;
+        const std::unique_ptr<juce::Drawable>& icon_off_;
+        const juce::String label_on_;
+        const juce::String label_off_;
         bool separate_on_off_looks_;
         bool use_icon_;
         //Box bounding_box_;
