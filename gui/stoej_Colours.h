b35102aa49b14edc3bc76935f86af2190c87150f
Lorenzo Panieri <gokk99@gmail.com>
2023-01-16 21:59:38 +0100

    cherry pick 82568f615e9cf7dc71f7410b53de291b436ffdf9 again

---
+++ gui/stoej_Colours.h
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    stoej_Colours.h
+    Created: 8 Jan 2023 8:14:54pm
+    Author:  Lorenzo
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+namespace stoej {
+    namespace Colours {
+        static const juce::Colour meta_null = juce::Colour(0x00000000);
+        static const juce::Colour meta_unassigned = juce::Colour(0x00ff00ff);
+    }
+}
